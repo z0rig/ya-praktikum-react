5c7e41fd983a2b5306cbff2370e40c2ff3f93216
igor_zorin <igor_zorin@mail.ru>
2021-05-29 10:19:02 +0300

    пишет развернутую проверку PropTypes для компонента BurgerIngredients

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -50,5 +50,21 @@ function BurgerIngredients({ ingredients }) {
 export default BurgerIngredients;
 
 BurgerIngredients.propTypes = {
-  ingredients: PropTypes.arrayOf(PropTypes.object)
+  ingredients: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({
+      _id: PropTypes.string,
+      name: PropTypes.string,
+      type: PropTypes.string,
+      proteins: PropTypes.number,
+      fat: PropTypes.number,
+      carbohydrates: PropTypes.number,
+      calories: PropTypes.number,
+      price: PropTypes.number,
+      image: PropTypes.string,
+      image_mobile: PropTypes.string,
+      image_large: PropTypes.string,
+      __v: PropTypes.number,
+    }))
+  }))
 };
