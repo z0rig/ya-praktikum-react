69b9dcfa81851b96eb52f71ae7cd07c30b25119c
Igor Zorin <igor_zorin@mail.ru>
2021-05-31 15:52:56 +0300

    исправляет ошибки в консоли. Не понятно почему _id посчитались как не уникальные, но что поделать

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -32,8 +32,8 @@ function BurgerIngredients ( { ingredients } ) {
                 <h3 className='text text_type_main-medium mb-6'>{ title }</h3>
 
                 <ul className={ styles.list }>
-                  { items.map( ( ingredient ) => (
-                    <li className={ styles.item } key={ ingredient._id }>
+                  { items.map( ( ingredient, idx ) => (
+                    <li className={ styles.item } key={ idx }>
                       <Counter count={ 1 } size="default" />
                       <IngredientCard ingredient={ ingredient } />
                     </li>
